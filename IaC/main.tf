2c6435a371be4e06d26ce8ff93d74b5d4827ec69
Joyson <jng05929@gmail.com>
2025-12-06 16:59:38 -0500

    Revert "feat: Configure IRSA, CloudWatch observability, Prometheus and Grafana"
    This reverts commit b88c90c1f00ee35929ef5d35a41c46bc6242a82d.

--- IaC/main.tf
+++ IaC/main.tf
@@ -6,25 +6,82 @@ data "aws_availability_zones" "available" {
 
 data "aws_caller_identity" "current" {}
 
-# Reference existing VPC
-data "aws_vpc" "existing" {
-  id = "vpc-0482006eacc05081c"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = ">= 4.0"
+
+  name = "eks-g3-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3) # pick 3 AZs
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = false
+  enable_dns_hostnames = true
+  enable_dns_support = true
 }
 
-# Reference existing private subnets
-data "aws_subnets" "private" {
-  filter {
-    name   = "subnet-id"
-    values = ["subnet-0e1c23f5bef0a0523", "subnet-04796fca1c11de44c", "subnet-0477efaa4cf9565fc"]
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.10.1"
+  kubernetes_version = "1.33"
+  name    = "g3-eks-cluster"
+  subnet_ids = module.vpc.private_subnets
+  vpc_id = module.vpc.vpc_id
+  
+  access_entries = {
+    cluster_admin = {
+      principal_arn = data.aws_caller_identity.current.arn
+
+      type = "STANDARD"
+
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type       = "cluster"
+            namespaces = []
+          }
+        }
+      }
+    }
+  }
+
+
+  # Control plane logging if desired
+  # enable_irsa = true
+  
+  # Create two managed node groups across at least 2 AZs.
+  eks_managed_node_groups = {
+    general-1 = {
+      name = "general-1"
+      instance_types = ["t3.medium", "t3.large", "t3a.medium"]
+      ami_type = "AL2023_x86_64_STANDARD"
+      desired_capacity = 2
+      min_size = 1
+      max_size = 3
+    }
+
+    general-2 = {
+      name = "general-2"
+      instance_types = ["t3.medium", "t3.large", "t3a.medium"]
+      ami_type = "AL2023_x86_64_STANDARD"
+      desired_capacity = 2
+      min_size = 1
+      max_size = 3
+    }
+  }
+
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
   }
 }
-
-# Reference existing EKS cluster (data source only, no module)
-data "aws_eks_cluster" "existing" {
-  name = "g3-eks-cluster"
-}
-
-data "aws_eks_cluster_auth" "existing" {
-  name = "g3-eks-cluster"
-}
-
